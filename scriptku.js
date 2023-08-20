12f5b8c295c266b1f6c7e7d7f49a73861b7d3881
riskise <thamsaliwa@gmail.com>
2023-08-20 17:40:35 +0700

    modifed js

--- scriptku.js
+++ scriptku.js
@@ -23,4 +23,49 @@ document.getElementById("toggleButton").addEventListener("click", function() {
   }
   
   setInterval(changeColor, 5000); // Mengganti warna setiap 5 detik
+
+
+  const data = {};
+const ctx = document.getElementById("voteChart").getContext("2d");
+
+function vote() {
+    const nameInput = document.getElementById("nameInput");
+    const name = nameInput.value.toLowerCase();
+
+    if (name.trim() === "") {
+        alert("Please enter a valid name.");
+        return;
+    }
+
+    data[name] = data[name] ? data[name] + 1 : 1;
+    updateChart();
+    nameInput.value = "";
+}
+
+function updateChart() {
+    const labels = Object.keys(data);
+    const values = Object.values(data);
+
+    const chart = new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: labels,
+            datasets: [{
+                label: "Votes",
+                data: values,
+                backgroundColor: "rgba(75, 192, 192, 0.2)",
+                borderColor: "rgba(75, 192, 192, 1)",
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
   
